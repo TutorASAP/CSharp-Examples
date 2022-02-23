461fee1ad4078c8db047c3628185548c23973d9f
DanielM-ASAP <95250823+DanielM-ASAP@users.noreply.github.com>
2022-02-23 19:44:13 +0100

    Update CSharpPredefinedTypes.cs

--- VariableDeclaration/CSharpPredefinedTypes.cs
+++ VariableDeclaration/CSharpPredefinedTypes.cs
@@ -6,18 +6,21 @@ ushort myUShort = 0; // 16 bit
 uint myUInt = 0u; // 32 bit
 ulong myULong = 0ul; // 64 bit
 #endregion
+
 #region SIGNED (WITH SYMBOL) INTEGEER VALUE TYPES
 sbyte mySByte = 0; // 8 bit
 short myShort = 0; // 16 bit
 int myInt = 0; // 32 bit
 long myLong = 0L; // 64 bit
 #endregion
+
 #region FLOATING POINT VALUE TYPES
 float myFloat = 0.0f; // 32 bit * MOST USED *
 double myDouble = 0.0d; // 64 bit
 decimal myDecimal = 0.0m; // 128 bit
 #endregion
+
 #region PREDEFINED REFERENCE TYPES
 string myString = "Hello World!"; //The size of the char array
-object myObj = new object();
+object myObj = new object(); //The size of the object
 #endregion
