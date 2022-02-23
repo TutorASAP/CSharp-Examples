6b842e3f39db2221ab4aa8e620053356405abf4a
Daniel Molinero Lucas <danielm@tutorasap.org>
2022-02-23 19:43:16 +0100

    Create CSharpPredefinedTypes.cs

---
+++ VariableDeclaration/CSharpPredefinedTypes.cs
@@ -0,0 +1,23 @@
+﻿#region UNSIGNED (WITH SYMBOL) INTEGEER VALUE TYPES
+bool myBool = false; // 8 bit
+byte myByte = 0; // 8 bit
+char myChar = 'A'; // (Unicode character) 16 bit
+ushort myUShort = 0; // 16 bit
+uint myUInt = 0u; // 32 bit
+ulong myULong = 0ul; // 64 bit
+#endregion
+#region SIGNED (WITH SYMBOL) INTEGEER VALUE TYPES
+sbyte mySByte = 0; // 8 bit
+short myShort = 0; // 16 bit
+int myInt = 0; // 32 bit
+long myLong = 0L; // 64 bit
+#endregion
+#region FLOATING POINT VALUE TYPES
+float myFloat = 0.0f; // 32 bit * MOST USED *
+double myDouble = 0.0d; // 64 bit
+decimal myDecimal = 0.0m; // 128 bit
+#endregion
+#region PREDEFINED REFERENCE TYPES
+string myString = "Hello World!"; //The size of the char array
+object myObj = new object();
+#endregion
