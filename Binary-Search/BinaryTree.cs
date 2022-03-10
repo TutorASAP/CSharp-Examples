e007abd5aeeb5620f1e9c4f1ba7c83cfe1249295
DanielM-ASAP <95250823+DanielM-ASAP@users.noreply.github.com>
2022-03-10 10:54:05 +0100

    Binary Search first commit

---
+++ Binary-Search/BinaryTree.cs
@@ -0,0 +1,86 @@
+﻿namespace BinaryTree
+{
+    public sealed partial class BinaryTree<T>
+    { 
+        public TreeNode? Root { get => root; }
+        private TreeNode? root = null;
+
+        private bool leftOrRightElection = true;
+
+        public List<TreeNode?>? GetChildren { get => root?.GetChildren(); }
+
+        public BinaryTree(TreeNode? root = null)
+        {
+            this.root = root;
+        }
+
+        public void Add(T value)
+        {
+            if (root == null)
+            {
+                root = new TreeNode(value, null);
+                return;
+            }
+            else if (root.left == null)
+            {
+                root.left = new TreeNode(value, root);
+                return;
+            }
+            else if (root.right == null)
+            {
+                root.right = new TreeNode(value, root);
+                return;
+            }
+            else
+            {
+                var currentRoot = root;
+                var lastCurrentRoot = currentRoot;
+
+                while (true)
+                {
+                    if (currentRoot == null)
+                    {
+                        currentRoot = new TreeNode(value, lastCurrentRoot);
+                        return;
+                    }
+                    else if (currentRoot.left == null)
+                    {
+                        currentRoot.left = new TreeNode(value, currentRoot);
+                        return;
+                    }
+                    else if (currentRoot.right == null)
+                    {
+                        currentRoot.right = new TreeNode(value, currentRoot);
+                        return;
+                    }
+                    else
+                    {
+                        lastCurrentRoot = currentRoot;
+
+                        if (leftOrRightElection)
+                        {
+                            currentRoot = currentRoot.left;
+                            leftOrRightElection = false;
+                        }
+                        else
+                        {
+                            currentRoot = currentRoot.right;
+                            leftOrRightElection = true;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        public void Remove(T value)
+        {
+
+        }
+
+        public TreeNode? Find(T value)
+        {
+            return null;
+        }
+    }
+}
---
+++ Binary-Search/TreeNode.cs
@@ -0,0 +1,51 @@
+﻿namespace BinaryTree
+{
+    public sealed partial class BinaryTree<T>
+    {
+        public class TreeNode
+        {
+            public TreeNode? left;
+            public TreeNode? right;
+            public TreeNode? parent;
+
+            public readonly bool IsRoot;
+
+            public readonly T Data;
+
+            public TreeNode(T data, TreeNode? parent, TreeNode? left = null, TreeNode? right = null)
+            {
+                this.parent = parent;
+                
+                this.left = left;
+                this.right = right;
+
+                Data = data;
+
+                IsRoot = parent == null ? true : false;
+            }
+
+            public List<TreeNode?> GetChildren()
+            {
+                var temp = new List<TreeNode?>
+                {
+                    this
+                };
+
+                if (left != null)
+                {
+                    var tempLeft = left.GetChildren();
+                    temp.AddRange(tempLeft);
+                }
+
+                if (right != null)
+                {
+                    var tempRight = right.GetChildren();
+                    temp.AddRange(tempRight);
+                }
+
+                return temp;
+            }
+        }
+    }
+    
+}
